0d177f87048fb3a8bb079e0f24cfc241734498bd
Farbod Jahan <farbods0@gmail.com>
2025-10-12 09:36:02 -0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // IMPORTANT: This line tells Tailwind which files to scan for classes
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}

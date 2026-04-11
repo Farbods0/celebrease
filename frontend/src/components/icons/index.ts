800ec60641d9b17ac568fdb54614c050f2993b02
MD Pahlovi <mdpahlovi@outlook.com>
2026-04-11 18:58:47 +0600

    feat: color variable update

---
+++ frontend/src/components/icons/index.ts
@@ -0,0 +1,2 @@
+export { default as ShoppingBasket } from "./shopping-basket";
+export { default as UserCircle } from "./user-circle";
--- frontend/src/components/ui/button.tsx
+++ frontend/src/components/ui/button.tsx
@@ -6,7 +6,7 @@ import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@/lib/utils";
 
 const buttonVariants = cva(
-    "inline-flex items-center justify-center rounded-md font-medium whitespace-nowrap outline-none select-none disabled:pointer-events-none disabled:opacity-50 [&_svg:not([class*='size-'])]:size-5",
+    "inline-flex items-center justify-center rounded-full font-medium whitespace-nowrap outline-none select-none disabled:pointer-events-none disabled:opacity-50 [&_svg:not([class*='size-'])]:size-5",
     {
         variants: {
             variant: {
